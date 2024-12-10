4cf62a9146492ef4552b9b3f963d1650da5f49bf
Taruu <taruumax@gmail.com>
2024-12-10 08:32:16 +0300

    add

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ values = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=["settings.toml", ".secrets.toml"],
 )
+
+# Current wiki Base url
+BASE_URL = values.get("hosts.BASE_URL")
+# use checks in hostname
+REPLACE_HOSTS_PATTERN_LIST = values("hosts.replace_hosts_pattern_list")
+

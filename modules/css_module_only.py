a1ea1bab322bb655e78452b31d43d947dee659bc
Taruu <taruumax@gmail.com>
2025-01-09 14:00:00 +0300

    css_module_only.py add

---
+++ modules/css_module_only.py
@@ -0,0 +1,19 @@
+from utls.base_utils import Article
+
+import re
+
+from utls.base_utils import Article
+from utls.regex_parser import s_bracket_dual_regex, get_module_css
+from config import logger
+
+
+def handle(article: Article) -> Article:
+    dual_blocks_list = re.findall(s_bracket_dual_regex, article.source_code)
+    dual_blocks_list = [block.lower() for block in dual_blocks_list]
+    module_only_css = [block for block in dual_blocks_list if ("module" in block) and ("css" in block)]
+    if len(module_only_css) > 1:
+        logger.error(f"TO MANY CSS BLOCKS {article.page_name}")
+        return article
+    list_css_modules = get_module_css(article.source_code)
+    article.source_code = list_css_modules[0]
+    return article
